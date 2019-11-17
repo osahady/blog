6a0676e626343f0001404bf2f972775bd4afdab6
osahady <osahady@localhost.localdomain>
2019-11-17 10:30:03 +0300

    media starting

---
+++ resources/views/admin/media/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    <h1>Media</h1>
+    @if ($photos)    
+        <table class="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Created</th>
+                <th scope="col">Updated</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($photos as $photo)
+                    <tr>
+                        <th scope="row">{{ $photo->id }}</th>
+                        <td><img src="{{ $photo->file }}" alt="" width="100"></td>
+                        <td>{{ $photo->created_at ? $photo->created_at->diffForHumans() : 'no date'}}</td>
+                        <td>{{ $photo->updated_at ? $photo->updated_at->diffForHumans() : 'no date'}}</td>
+                    </tr> 
+                @endforeach
+              
+              
+            </tbody>
+          </table>
+    @endif
+
+@stop
--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,8 @@ Route::group(['middleware'=> 'admin'], function(){
     Route::resource('admin/users', 'AdminUsersController');
     Route::resource('admin/posts', 'AdminPostsController');
     Route::resource('admin/categories', 'AdminCategoriesController');
+    Route::resource('admin/media', 'AdminMediaController');
+    Route::get('admin/media/upload', 'AdminMediaController@store')->name('media.upload');
     
 });
 
